764c33e3420fca65e2cd1c659e965a7534932700
Sisilisko <beloborodova.viktoriia@gmail.com>
2022-10-13 17:43:47 +0300

    fix: disabled

--- Ryhmätyöt/Projekti2-arvauspeli-ENG/script.js
+++ Ryhmätyöt/Projekti2-arvauspeli-ENG/script.js
@@ -10,15 +10,15 @@ function arvaa(arpa) {
   var arvaus = arpa.value;
   if ((kerrat < 3)&&(win==0)) {
     if (arvaus < arvoLuku) {
-      document.getElementById('viesti').innerHTML = 'Valitsit liian pienen numeron';
+      document.getElementById('viesti').innerHTML = 'Your guess is too low.';
       document.getElementById(''+arvaus+'').classList.add("vaarin");
       document.getElementById(''+arvaus+'').disabled = true;
     } else if(arvaus > arvoLuku) {
-      document.getElementById('viesti').innerHTML = 'Valitsit liian suuren numeron';
+      document.getElementById('viesti').innerHTML = 'Your guess is too high.';
       document.getElementById(''+arvaus+'').classList.add("vaarin");
       document.getElementById(''+arvaus+'').disabled = true;
     } else if (arvaus == arvoLuku) {
-      document.getElementById('viesti').innerHTML ='Oikein, käytit ' + kerrat + ' kertaa<br><button class="taas" onclick="pelaaTaas()">Pelaa taas</button>';
+      document.getElementById('viesti').innerHTML ='Win! It took you ' + kerrat + ' turns.<br><button class="taas" onclick="pelaaTaas()">Play again</button>';
       document.getElementById(''+arvoLuku+'').classList.toggle("oikein");
       win = 1;
       winNumero += 1;
@@ -26,28 +26,29 @@ function arvaa(arpa) {
     }
   } else if ((kerrat==3)&&(win==0)) {
     if (arvaus < arvoLuku) {
-      document.getElementById('viesti').innerHTML = 'Valitsit liian pienen numeron ja käytit kaikki arvauksesi,<br> oikea luku on ' + arvoLuku + '<br><button class="taas" onclick="pelaaTaas()">Pelaa taas</button>';
+      document.getElementById('viesti').innerHTML = 'You guess is too low and no turns left.<br> The correct number is ' + arvoLuku + '.<br><button class="taas" onclick="pelaaTaas()">Play again</button>';
       document.getElementById(''+arvaus+'').classList.add("vaarin");
       document.getElementById(''+arvaus+'').disabled = true;
       loseNumero += 1;
       document.getElementById('loseNumber').innerHTML = loseNumero;
       document.getElementById(''+arvoLuku+'').classList.add("levitate");
+      document.getElementById(''+arvaus+'').disabled = true;
     } else if(arvaus > arvoLuku) {
-      document.getElementById('viesti').innerHTML = 'Valitsit liian suuren numeron ja käytit kaikki arvauksesi,<br> oikea luku on ' + arvoLuku + '<br><button class="taas" onclick="pelaaTaas()">Pelaa taas</button>';
-      document.getElementById(''+arvaus+'').classList.add("vaarin");
-      document.getElementById(''+arvaus+'').disabled = true;
+      document.getElementById('viesti').innerHTML = 'You guess is too high and no turns left.<br> The correct number is ' + arvoLuku + '.<br><button class="taas" onclick="pelaaTaas()">Play again</button>';
+      document.getElementById(''+arvaus+'').classList.toggle("vaarin");
       loseNumero += 1;
       document.getElementById('loseNumber').innerHTML = loseNumero;
       document.getElementById(''+arvoLuku+'').classList.add("levitate");
+      document.getElementById(''+arvaus+'').disabled = true;
     } else if (arvaus == arvoLuku) {
-      document.getElementById('viesti').innerHTML ='Oikein, käytit ' + kerrat + ' kertaa<br><button class="taas" onclick="pelaaTaas()">Pelaa taas</button>';
+      document.getElementById('viesti').innerHTML ='Win! It took you ' + kerrat + ' turns.<br><button class="taas" onclick="pelaaTaas()">Play again</button>';
       document.getElementById(''+arvoLuku+'').classList.toggle("oikein");
       win = 1;
       winNumero += 1;
       document.getElementById('winNumber').innerHTML = winNumero;
     }
   } else if (win==1){
-      document.getElementById('viesti').innerHTML ='Löysit jo oikean luvun, se on ' +arvoLuku + '<br><button class="taas" onclick="pelaaTaas()">Pelaa taas</button>';
+      document.getElementById('viesti').innerHTML ='You already found the right number, that is ' +arvoLuku + '.<br><button class="taas" onclick="pelaaTaas()">Play again</button>';
       document.getElementById(''+arvoLuku+'').classList.add("oikein");
   }
 }
